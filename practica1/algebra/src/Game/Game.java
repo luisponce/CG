082208dba1e02c2201f8cdb2afd835646a1d3828
Jonathan Eidelman Manevich <jonyxmaster@gmail.com>
2015-03-14 16:35:59 -0500

    Made the asteroids level to be random when created

--- practica1/algebra/src/Game/Game.java
+++ practica1/algebra/src/Game/Game.java
@@ -197,7 +197,9 @@ public class Game extends Canvas {
                 y = (int) seed.nextInt(h-100)+100;
             }
             
-            Asteroid enemy = new Asteroid(x,y, v, e, 0, 0, g,3);
+            int level = (seed.nextInt() % 3) + 1;
+            
+            Asteroid enemy = new Asteroid(x,y, v, e, 0, 0, g,level);
             
             dir = new Vector2(new Punto2(x,y),
                     new Punto2(w/2 + seed.nextInt(50)-50, h/2 + seed.nextInt(50)-50));
