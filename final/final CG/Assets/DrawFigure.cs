1f06bccee1c409cb30245820a17d85df9ab22c58
luis M Ponce <luisponcekun@gmail.com>
2015-06-01 11:19:11 -0500

    added point movement script

---
+++ final/final CG/Assets/DrawFigure.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DrawFigure : MonoBehaviour {
+
+	Vector3[] vertex = new Vector3[] {new Vector3(0,0,0), new Vector3(5,0,0), new Vector3(0,0,5), new Vector3(5,5,5)};
+
+	public GameObject vertexSphere;
+
+	// Use this for initialization
+	void Start () {
+		for(int i = 0; i<vertex.Length; i++){
+			GameObject obj = (GameObject) GameObject.Instantiate(vertexSphere, vertex[i], Quaternion.identity);
+			obj.transform.parent = this.transform;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ final/final CG/Assets/Point.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Point : MonoBehaviour {
+
+	Vector3 initialPosition = new Vector3(2,2,2);
+
+	float speed = 1f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 dir = new Vector3(0,0,0);
+
+		if(Input.GetKey(KeyCode.A)){
+			//-x
+			dir.x = -1;
+		}
+		if(Input.GetKey(KeyCode.D)){
+			//+x
+			dir.x = 1;
+		}
+
+		if(Input.GetKey(KeyCode.W)){
+			//+y
+			dir.y = 1;
+		}
+		if(Input.GetKey(KeyCode.S)){
+			//-y
+			dir.y = -1;
+		}
+
+		if(Input.GetKey(KeyCode.O)){
+			//-z
+			dir.z = 1;
+		}
+		if(Input.GetKey(KeyCode.L)){
+			//+z
+			dir.z = -1;
+		}
+
+		transform.Translate(dir * speed * Time.deltaTime);
+	}
+}
